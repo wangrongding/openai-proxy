ee4cfd5a440cf744f5477283004398dec7c1aade
荣顶 <wangrongding@qq.com>
2024-01-29 01:15:45 +0800

    Update deno.ts

--- deno.ts
+++ deno.ts
@@ -5,9 +5,9 @@ Deno.serve(async (request) => {
   url.host = OPENAI_API_HOST;
 
   const newRequest = new Request(url.toString(), {
-    headers: request.headers,
-    method: request.method,
-    body: request.body,
+    // headers: request.headers,
+    // method: request.method,
+    // body: request.body,
     redirect: "follow",
   });
   return await fetch(newRequest);
